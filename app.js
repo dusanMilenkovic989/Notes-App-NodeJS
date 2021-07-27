666a091883bc278501e503ab7f0c599d6f0abb65
dusanMilenkovic989 <dusan.milenkovic989@gmail.com>
2021-07-27 08:40:50 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Customize add command
+yargs.command({
+    command: 'add',
+    describe: 'Way to add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,                                     
+            type: 'string'                                          
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler({ title, body }) {
+        notes.addNotes(title, body)
+    }
+})
+
+// Customize remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Way to remove the note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler({ title }) {
+        notes.removeNotes(title)
+    }
+})
+
+// Customize list command
+yargs.command({
+    command: 'list',
+    describe: 'Way to list all of your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Customize read command
+yargs.command({
+    command: 'read',
+    describe: 'Way to read the note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler({ title }) {
+        notes.readNotes(title)
+    }
+})
+
+
+// console.log(yargs.argv)                                   
+yargs.parse()
---
+++ notes.js
@@ -0,0 +1,86 @@
+'use strict'
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+let notes = []
+
+const getNote = (title) => notes.find((note) => note.title.toLowerCase() === title.toLowerCase())
+
+const addNotes = (title, body) => {
+    const titleTrimmed = title.trim()
+    const note = getNote(titleTrimmed)
+
+    if (!note && titleTrimmed.length > 0) {
+        notes.push({
+            title: titleTrimmed,
+            body: body.trim()
+        })
+        saveNotes()
+        console.log(chalk.green.inverse('Note added'))
+    } else {
+        console.log(chalk.red.inverse('Please provide a note with a different title. This one already exists, or you have not named your title'))
+    }
+}
+
+const removeNotes = (title) => {
+    const note = getNote(title.trim())
+
+    if (note) {
+        const noteIndex = notes.findIndex((item) => item.title === note.title)      // small change in type style to make it possible for note to be found in regards to the real note title, and not in regards to the argument typed, so that the argument could be case insensitive
+        notes.splice(noteIndex, 1)
+        saveNotes()
+        console.log(chalk.green.inverse(`Note: '${note.title}' has been removed from the system`))
+    } else {
+        console.log(chalk.red.inverse('Note under this title does not exist'))
+    }
+}
+
+const listNotes = () => {
+    if (notes.length > 0) {
+        const plural = notes.length !== 1 ? 's' : ''
+
+        console.log(chalk.green.inverse(`Your note${plural}:`))
+
+        notes.forEach((note) => {
+            console.log(chalk.green.bold(note.title))
+        })
+    } else {
+        console.log(chalk.red.inverse('There are no notes yet'))
+    }
+}
+
+const readNotes = (title) => {
+    const note = getNote(title.trim())
+
+    if (note) {
+        console.log(chalk.green.bold(note.title))
+        console.log(chalk.bold(note.body))
+    } else {
+        console.log(chalk.red.inverse('There is no note with provided title'))
+    }
+}
+
+const saveNotes = () => {
+    const notesJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', notesJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const notesBuffer = fs.readFileSync('notes.json')
+        const notesJSON = notesBuffer.toString()
+        return JSON.parse(notesJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+notes = loadNotes()
+
+module.exports = {
+    addNotes,
+    removeNotes,
+    listNotes,
+    readNotes,
+}
